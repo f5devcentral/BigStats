e8609931aa83cb59001635d1b703609a092a84e6
Nathan Pearce <nathan.pearce@gmail.com>
2018-09-29 01:16:10 -0700

    re-re-refactor

--- SRC/BigStats/nodejs/bigstats-settings.js
+++ SRC/BigStats/nodejs/bigstats-settings.js
@@ -12,19 +12,12 @@ const logger = require('f5-logger').getInstance();
 const os = require('os');
 const Ajv = require('ajv');
 const Util = require('./util');
-<<<<<<< HEAD
 const util = new Util('BigStatsSettings');
 
-=======
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
 const BigStatsSettingsSchema = require('./bigstats-schema.json');
 
 class BigStatsSettings {
   constructor () {
-<<<<<<< HEAD
-=======
-    this.util = new Util('BigStatsSettings');
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
     this.state = {};
   }
 
@@ -35,15 +28,9 @@ class BigStatsSettings {
     var that = this;
     this.loadState(null, function (err, state) {
       if (err) {
-<<<<<<< HEAD
         logger.info(`[BigStatsSettings - ERROR] Error loading state: ${err}`);
       } else {
         logger.info('[BigStatsSettings] State loaded.');
-=======
-        error(this.util.formatLogMessage(`Error loading state: ${err}`));
-      } else {
-        logger.info(this.util.formatLogMessage('State loaded.'));
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
         that.state = state;
       }
     });
@@ -54,7 +41,6 @@ class BigStatsSettings {
    * handle onGet HTTP request
    */
   onGet (restOperation) {
-<<<<<<< HEAD
     this.getHostVersion()
     .then((version) => {
 
@@ -83,14 +69,6 @@ class BigStatsSettings {
         reject('\n\ngetHostVersion(): ERR' +err);
       });
     });
-=======
-    let hostname = os.hostname();
-    let safeHostname = hostname.replace(/\./g, '-');
-    this.state.config.hostname = safeHostname;
-    // Respond with the persisted state (config)
-    restOperation.setBody(this.state);
-    this.completeRestOperation(restOperation);
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
   }
   /**
    * handle onPost HTTP request
@@ -100,20 +78,12 @@ class BigStatsSettings {
     let newState = this.validateConfiguration(input);
     if (newState) {
       this.state = newState;
-<<<<<<< HEAD
       logger.info('[BigStatsSettings] Settings updated.');
-=======
-      logger.info(this.util.formatLogMessage('Settings updated.'));
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
       restOperation.setBody(this.state);
       this.completeRestOperation(restOperation);
     } else {
       // Invalid input
-<<<<<<< HEAD
       restOperation.fail(new Error('[BigStatsSettings - ERROR] Error: Invalid/No state provided...'));
-=======
-      restOperation.fail(new Error(this.util.formatLogMessage('Error: Invalid/No state provided...')));
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
     }
   }
   /**
@@ -131,19 +101,11 @@ class BigStatsSettings {
       let validate = ajv.compile(BigStatsSettingsSchema);
       let valid = validate(jsonInput);
       if (valid === false) {
-<<<<<<< HEAD
         const error = util.safeAccess(() => validate.errors[0].message, '');
         if (error !== '') {
           logger.info(`[BigStatsSettings] Validation error: ${this.translateAjvError(validate.errors[0])}`);
         } else {
           logger.info('[BigStatsSettings] Unknown validation error.');
-=======
-        const error = this.util.safeAccess(() => validate.errors[0].message, '');
-        if (error !== '') {
-          logger.info(this.util.formatLogMessage(`Validation error: ${this.translateAjvError(validate.errors[0])}`));
-        } else {
-          logger.info(this.util.formatLogMessage('Unknown validation error.'));
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
         }
         return false;
       }
@@ -165,17 +127,12 @@ class BigStatsSettings {
       try {
         input = JSON.parse(input);
       } catch (err) {
-<<<<<<< HEAD
         logger.info(`[BigStatsSettings - ERROR] Error: Unable to parse input: ${err}`);
-=======
-        logger.info(this.util.formatLogMessage(`Error: Unable to parse input: ${err}`));
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
         return;
       }
     }
     return input;
   }
-<<<<<<< HEAD
 
   createRestOperation (uri, body) {
 
@@ -190,25 +147,6 @@ class BigStatsSettings {
 
     return restOp;
 
-=======
-  /**
-   * handle /example HTTP request
-   */
-  getExampleState () {
-    return {
-      'config': {
-        'destination': {
-          'protocol': '[http|https|statsd|kafka]',
-          'address': '[ip_address]',
-          'port': '[tcp_port]',
-          'uri': '[uri]'
-        },
-        'interval': '[seconds]',
-        'enabled': true,
-        'debug': false
-      }
-    };
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
   }
 
   /**
@@ -236,8 +174,4 @@ BigStatsSettings.prototype.isPublic = true;
 BigStatsSettings.prototype.isSingleton = true;
 BigStatsSettings.prototype.isPersisted = true;
 
-<<<<<<< HEAD
 module.exports = BigStatsSettings;
-=======
-module.exports = BigStatsSettings;
->>>>>>> 96b6e7c23ec16b4291e3aec19973d1de5bd3208e
